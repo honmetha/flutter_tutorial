ff9f2b869a75d6a8ff6c67d97ecd6650154cae30
Hon <dennis.mejen@gmail.com>
2020-09-16 23:08:46 +0700

    [Added] Add 50% of Flutter Crash Course

---
+++ wordpair_generator/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: Scaffold(
+            appBar: AppBar(title: Text('WordPair Generator')),
+            body: Center(
+              child: Text('Hello World'),
+            )));
+  }
+}
