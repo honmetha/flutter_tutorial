f2cde644485c627cc5872cff769106482ed8afec
Hon <dennis.mejen@gmail.com>
2021-11-27 23:12:32 +0700

    [Added] 8 - Images & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello ninjas!',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'
-          )
-        ),
+        child: Image.asset('assets/home-2.jpeg')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
