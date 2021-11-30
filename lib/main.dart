51e6422604a6331492136b62b33eb57bffb2a442
Hon <dennis.mejen@gmail.com>
2021-12-01 00:06:49 +0700

    [Added] 15 - Ninja ID Project (Part 2)

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,16 @@ class NinjaCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpeg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
             const Text(
               'NAME',
               style: TextStyle(
@@ -26,6 +36,7 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
+            const SizedBox(height: 10.0),
             Text(
               'Chun-Li',
               style: TextStyle(
@@ -34,6 +45,42 @@ class NinjaCard extends StatelessWidget {
                 fontSize: 20.0,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            const SizedBox(height: 30.0),
+            const Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10.0),
+                Text(
+                  'chun.li@thenetninja.co.uk',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
