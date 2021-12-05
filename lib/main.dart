d34ec3b8500f156bce258447b00002e100bc3c6c
Hon <dennis.mejen@gmail.com>
2021-12-05 23:19:12 +0700

    [Added] 22 - Starting the World Time App

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,6 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:flutter_tutorial/pages/home.dart';
 
 void main() => runApp(const MaterialApp(
-  home: QuoteList(),
-)); 
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(author: 'Charles Darwin', text: 'A man who dares to waste one hour of time has not discovered the value of life.'),
-    Quote(author: 'Charles Darwin', text: 'It is not the strongest of the species that survives, nor the most intelligent that survives. It is the one that is most adaptable to change.'),
-    Quote(author: 'Albert Einstein', text: 'There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: const Text('Awesome Quotes'),
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: () {
-            setState(() {
-              quotes.remove(quote);
-            });
-          },
-        )).toList(),
-      ),
-    );
-  }
-}
+  home: Home(),
+));
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('choose location screen'),
+    );
+  }
+}
