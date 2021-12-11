950e27c45a2834f65e4bbaa84b75e61e8e763187
Hon <dennis.mejen@gmail.com>
2021-12-11 23:03:04 +0700

    [Added] 28 - WorldTime Custom Class (Part 1)

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -10,23 +8,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // make the request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Bangkok'));
-    Map date = jsonDecode(response.body);
-    // print(date);
-
-    // get properties from data
-    String datetime = date['datetime'];
-    String offset = date['utc_offset'].substring(1, 3);
-    // print(datetime);
-    // print(offset);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-  }
 
   @override
   void initState() {
---
+++ lib/services/world_time.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for the UI
+  late String time; // the time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+
+  WorldTime({ required this.location, required this.flag, required this.url });
+
+  void getTime() async {
+    // make the request
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map date = jsonDecode(response.body);
+    // print(date);
+
+    // get properties from data
+    String datetime = date['datetime'];
+    String offset = date['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set the time property
+    time = now.toString();
+  }
+}
