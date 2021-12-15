9cb5b8a8ea4a75d6b4b22f4d2ac85f5e8aa1d8e0
Hon <dennis.mejen@gmail.com>
2021-12-16 02:07:25 +0700

    [Added] 31 - Formatting & Showing Dates

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,16 +17,43 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            TextButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: const Icon(Icons.edit_location),
-              label: const Text('Edit Location'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: const Icon(Icons.edit_location),
+                label: const Text('Edit Location'),
+              ),
+              const SizedBox(
+                height: 20.0
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: const TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0
+              ),
+              Text(
+                data['time'],
+                style: const TextStyle(
+                  fontSize: 66.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
 
@@ -28,7 +29,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       // set the time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
       print('caught error: $e');
       time = 'could not get time data';
