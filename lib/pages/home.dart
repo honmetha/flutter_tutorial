f0614cdf08e00844e6fc268b9bcbc62f01c0e211
Hon <dennis.mejen@gmail.com>
2021-12-18 23:38:58 +0700

    [Added] 33 -Ternary Operators (Part 2)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,45 +17,65 @@ class _HomeState extends State<Home> {
 
     // set background
     String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700]!;
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: const Icon(Icons.edit_location),
-                label: const Text('Edit Location'),
-              ),
-              const SizedBox(
-                height: 20.0
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
-                    style: const TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(
+                      color: Colors.grey[300],
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 20.0
-              ),
-              Text(
-                data['time'],
-                style: const TextStyle(
-                  fontSize: 66.0,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 20.0
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: const TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20.0
+                ),
+                Text(
+                  data['time'],
+                  style: const TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
